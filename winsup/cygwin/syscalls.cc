cygwin/cygwin

commit 4b8222983f914a7950987802e73fc069dffc3058

Cygwin: fix errno values set by readlinkat
readlinkat(fd, "", ...) is supposed to return ENOENT per POSIX,
but Cygwin returns EBADF.

At the same time, we have to maintain the special feature of
glibc that readlinkat(fd, "", ...) operates on fd, if fd is pointing
at a symlink opened with O_PATH|O_NOFOLLOW.

And, while fixing that, readlinkat(fd, path, ...) *still* has to set errno
to EBADF, if fd is an invalid descriptor *and* path is a relative path.

This required to change the evaluation order in the helper function
gen_full_path_at.

Last but not least, in case of the aforementioned glibc-like special
handling for symlink descriptors, we have to make sure that errors from
gen_full_path_at are not spilled into that special handling.

Fixes: 6cc05784e16a ("Cygwin: readlinkat: allow pathname to be empty")
Reported-by: Bruno Haible <bruno@clisp.org>
Signed-off-by: Corinna Vinschen <corinna@vinschen.de>

winsup/cygwin/syscalls.cc
@@ -4415,19 +4415,6 @@ gen_full_path_at (char *path_ret, int dirfd, const char *pathname,
       set_errno (EFAULT);
       return -1;
     }
-  if (pathname)
-    {
-      if (!*pathname)
-	{
-	  set_errno (ENOENT);
-	  return -1;
-	}
-      if (strlen (pathname) >= PATH_MAX)
-	{
-	  set_errno (ENAMETOOLONG);
-	  return -1;
-	}
-    }
   if (pathname && isabspath_strict (pathname))
     stpcpy (path_ret, pathname);
   else
@@ -4459,6 +4446,16 @@ gen_full_path_at (char *path_ret, int dirfd, const char *pathname,
 	}
       if (pathname)
 	{
+	  if (!*pathname)
+	    {
+	      set_errno (ENOENT);
+	      return -1;
+	    }
+	  if (strlen (pathname) >= PATH_MAX)
+	    {
+	      set_errno (ENAMETOOLONG);
+	      return -1;
+	    }
 	  if (p[-1] != '/')
 	    *p++ = '/';
 	  stpcpy (p, pathname);
@@ -4803,21 +4800,26 @@ readlinkat (int dirfd, const char *__restrict pathname, char *__restrict buf,
   __try
     {
       char *path = tp.c_get ();
+      int save_errno = errno;
       int res = gen_full_path_at (path, dirfd, pathname);
       if (res)
 	{
-	  if (errno != ENOENT)
+	  if (errno != ENOENT && errno != ENOTDIR)
 	    __leave;
 	  /* pathname is an empty string.  This is OK if dirfd refers
 	     to a symlink that was opened with O_PATH | O_NOFOLLOW.
-	     In this case, readlinkat operates on the symlink. */
+	     In this case, readlinkat operates on the symlink.
+	     Don't propagate errors from gen_full_path_at after this point. */
+	  errno = save_errno;
 	  cygheap_fdget cfd (dirfd);
-	  if (cfd < 0)
-	    __leave;
-	  if (!(cfd->issymlink ()
+	  if (cfd < 0
+	      || (!(cfd->issymlink ()
 		&& cfd->get_flags () & O_PATH
-		&& cfd->get_flags () & O_NOFOLLOW))
-	    __leave;
+		&& cfd->get_flags () & O_NOFOLLOW)))
+	    {
+	      set_errno (ENOENT);
+	      __leave;
+	    }
 	  strcpy (path, cfd->get_name ());
 	}
       return readlink (path, buf, bufsize);

winsup/cygwin/release/3.4.7
@@ -18,3 +18,6 @@ Bug Fixes
 
 - Fix return value of ilogbl(NaN).
   Addresses: https://cygwin.com/pipermail/cygwin/2023-April/253511.html
+
+- Fix error handling in readlinkat.
+  Addresses: https://cygwin.com/pipermail/cygwin/2023-April/253510.html

dscho/msys2-runtime

commit 072b5c57a76c935d79a321526e0c60a1383d22c4

fchmodat/fstatat: fix regression with empty `pathname`
In 4b8222983f (Cygwin: fix errno values set by readlinkat, 2023-04-18)
the code of `readlinkat()` was adjusted to align the `errno` with Linux'
behavior.

To accommodate for that, the `gen_full_path_at()` function was modified,
and the caller was adjusted to expect either `ENOENT` or `ENOTDIR` in
the case of an empty `pathname`, not just `ENOENT`.

However, `readlinkat()` is not the only caller of that helper function.

And while most other callers simply propagate the `errno` produced by
`gen_full_path_at()`, two other callers also want to special-case empty
`pathnames` much like `readlinkat()`: `fchmodat()` and `fstatat()`.

Therefore, these two callers need to be changed to expect `ENOTDIR` in
case of an empty `pathname`, too.

I noticed this issue when one of my workflows failed consistently
while trying to untar an archive containing a symbolic link and
claiming this:

    Cannot change mode to rwxr-xr-x: Not a directory

With this here fix, things start working as expected again.

Fixes: 4b8222983f (Cygwin: fix errno values set by readlinkat, 2023-04-18)
Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

winsup/cygwin/syscalls.cc
@@ -4580,7 +4580,7 @@ fchownat (int dirfd, const char *pathname, uid_t uid, gid_t gid, int flags)
       int res = gen_full_path_at (path, dirfd, pathname);
       if (res)
 	{
-	  if (!(errno == ENOENT && (flags & AT_EMPTY_PATH)))
+	  if (!((errno == ENOENT || errno == ENOTDIR) && (flags & AT_EMPTY_PATH)))
 	    __leave;
 	  /* pathname is an empty string.  Operate on dirfd. */
 	  if (dirfd == AT_FDCWD)
@@ -4625,7 +4625,7 @@ fstatat (int dirfd, const char *__restrict pathname, struct stat *__restrict st,
       int res = gen_full_path_at (path, dirfd, pathname);
       if (res)
 	{
-	  if (!(errno == ENOENT && (flags & AT_EMPTY_PATH)))
+	  if (!((errno == ENOENT || errno == ENOTDIR) && (flags & AT_EMPTY_PATH)))
 	    __leave;
 	  /* pathname is an empty string.  Operate on dirfd. */
 	  if (dirfd == AT_FDCWD)