92099b1eafabd2a34622a7490acabb10ad028613
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-06-22 15:19:12 +0200

    Revert "Cygwin: pipe: Switch pipe mode to blocking mode by default"
    This reverts commit 7ed9adb356df61114bb3fa3d0efc098c0501921c.

--- winsup/cygwin/dtable.cc
+++ winsup/cygwin/dtable.cc
@@ -410,8 +410,9 @@ dtable::init_std_file_from_handle (int fd, HANDLE handle)
 	{
 	  fhandler_pipe *fhp = (fhandler_pipe *) fh;
 	  fhp->set_pipe_buf_size ();
-	  /* Set pipe always blocking */
-	  fhp->set_pipe_non_blocking (false);
+	  /* Set read pipe always to nonblocking */
+	  fhp->set_pipe_non_blocking (fhp->get_device () == FH_PIPER ?
+				      true : fhp->is_nonblocking ());
 	}
 
       if (!fh->open_setup (openflags))
