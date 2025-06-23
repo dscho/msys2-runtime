picolibc/picolibc

commit cbfaeba4f77b98219579b7569604de29e57cc9b9

Cygwin: pipe: Fix incorrect write length in raw_write()
If the write length is more than the pipe space in non-blocking
mode, the write length is wrongly set to 65536. This causes access
violation. This patch fixes that.

Fixes: 7ed9adb356df ("Cygwin: pipe: Switch pipe mode to blocking mode by default")
Signed-off-by: Takashi Yano <takashi.yano@nifty.ne.jp>

winsup/cygwin/fhandler/pipe.cc
@@ -532,10 +532,8 @@ fhandler_pipe_fifo::raw_write (const void *ptr, size_t len)
 	}
     }
 
-  if (len <= (size_t) avail || pipe_buf_size == 0)
+  if (len <= (size_t) avail)
     chunk = len;
-  else if (is_nonblocking ())
-    chunk = len = pipe_buf_size;
   else
     chunk = avail;
 
@@ -555,7 +553,7 @@ fhandler_pipe_fifo::raw_write (const void *ptr, size_t len)
       ULONG len1;
       DWORD waitret = WAIT_OBJECT_0;
 
-      if (left > chunk)
+      if (left > chunk && !is_nonblocking ())
 	len1 = chunk;
       else
 	len1 = (ULONG) left;

dscho/msys2-runtime

commit 01f493c84eaa8a42fa62788edd1226f05c2b8e69

pipe: fix SSH hang (again)
The recent changes in Cygwin's pipe logic are a gift that keeps on
giving.

The recurring pattern is that bugs are fixed, but with many of these bug
fixes, new bugs are produced, and we are stuck in this seemingly endless
tunnel of fixing bugs caused by bug fixes.

In cbfaeba4f7 (Cygwin: pipe: Fix incorrect write length in raw_write(),
2024-11-06), for example, a segmentation fault was fixed. Which is good!
However, a bug was introduced, where e.g. Git for Windows' `git clone`
via SSH frequently hangs indefinitely for large-ish repositories, see
https://github.com/git-for-windows/git/issues/5688.

That commit removed logic whereby in non-blocking mode, not only the
chunk size, but also the overall count of bytes to write (`len`) was
clamped to whatever is indicated as the `WriteQuotaAvailable`. Now only
`chunk` is clamped to that, but `len` is left at its original number.
However, the following `while` loop expects `len - chunk` (which is
assigned to `len1`) not to be positive in non-blocking mode.

Let's reinstate that clamping logic and implicitly fix this SSH hang.

Fixes: cbfaeba4f7 (Cygwin: pipe: Fix incorrect write length in raw_write(), 2024-11-06)
Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

winsup/cygwin/fhandler/pipe.cc
@@ -542,6 +542,8 @@ fhandler_pipe_fifo::raw_write (const void *ptr, size_t len)
 
   if (len <= (size_t) avail)
     chunk = len;
+  else if (is_nonblocking ())
+    chunk = len = avail;
   else
     chunk = avail;
 